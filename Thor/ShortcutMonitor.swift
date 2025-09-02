04b9a2292ddf971e77de391234028dc050272da2
Jonas Diemer <jonasdiemer@gmail.com>
2025-09-02 19:12:44 +0200

    make repeated activation of shortcut toggle through windows instead of minimizing

--- Thor/ShortcutMonitor.swift
+++ Thor/ShortcutMonitor.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2016 AlvinZhu. All rights reserved.
 //
 
-import Foundation
+import Carbon.HIToolbox
 import Cocoa
+import Foundation
 import MASShortcut
 
 struct ShortcutMonitor {
@@ -15,28 +16,44 @@ struct ShortcutMonitor {
     static func register() {
         let apps = AppsManager.manager.selectedApps
         for app in apps where app.shortcut != nil {
-            MASShortcutMonitor.shared().register(app.shortcut, withAction: {
-                guard defaults[.EnableShortcut] else { return }
+            MASShortcutMonitor.shared().register(
+                app.shortcut,
+                withAction: {
+                    guard defaults[.EnableShortcut] else { return }
 
-                if let frontmostAppIdentifier = NSWorkspace.shared.frontmostApplication?.bundleIdentifier,
-                    let targetAppIdentifier = Bundle(url: app.appBundleURL)?.bundleIdentifier,
-                    frontmostAppIdentifier == targetAppIdentifier {
-                    NSRunningApplication.runningApplications(withBundleIdentifier: frontmostAppIdentifier).first?.hide()
-                } else {
-                    if #available(macOS 10.15, *) {
-                        let configuration = NSWorkspace.OpenConfiguration()
-                        configuration.activates = true
-                        NSWorkspace.shared.openApplication(at: app.appBundleURL,
-                                                           configuration: configuration) { _, error in
-                            if let error = error {
-                                NSLog("ERROR: \(error)")
-                            }
-                        }
+                    if let frontmostAppIdentifier = NSWorkspace.shared.frontmostApplication?
+                        .bundleIdentifier,
+                        let targetAppIdentifier = Bundle(url: app.appBundleURL)?.bundleIdentifier,
+                        frontmostAppIdentifier == targetAppIdentifier
+                    {
+                        let source = CGEventSource(stateID: .hidSystemState)
+                        let keyGrave = CGKeyCode(kVK_ANSI_Grave)  // Key code for `~` on US keyboards
+                        let keyDown = CGEvent(
+                            keyboardEventSource: source, virtualKey: keyGrave, keyDown: true)
+                        keyDown?.flags = .maskCommand
+                        let keyUp = CGEvent(
+                            keyboardEventSource: source, virtualKey: keyGrave, keyDown: false)
+                        keyUp?.flags = .maskCommand
+
+                        keyDown?.post(tap: .cghidEventTap)
+                        keyUp?.post(tap: .cghidEventTap)
                     } else {
-                        NSWorkspace.shared.launchApplication(app.appBundleURL.lastPathComponent)
+                        if #available(macOS 10.15, *) {
+                            let configuration = NSWorkspace.OpenConfiguration()
+                            configuration.activates = true
+                            NSWorkspace.shared.openApplication(
+                                at: app.appBundleURL,
+                                configuration: configuration
+                            ) { _, error in
+                                if let error = error {
+                                    NSLog("ERROR: \(error)")
+                                }
+                            }
+                        } else {
+                            NSWorkspace.shared.launchApplication(app.appBundleURL.lastPathComponent)
+                        }
                     }
-                }
-            })
+                })
         }
     }
 
